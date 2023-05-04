30119729033864f482c1b3642508bc8a6a7c330f
pumkinni <cindy4766@naver.com>
2023-05-04 15:22:03 +0900

    Initial commit

---
+++ src/main/java/com/example/usedtransaction/UsedTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.usedtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usedtransaction/UsedTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usedtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
